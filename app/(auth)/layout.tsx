ccc1ce8c5a6a92a929ee850ee2a210f2c741a811
ivandohan <srgivando@gmail.com>
2023-09-21 00:41:18 +0700

    Adding simple auth and theme mode

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+const AuthLayout = ({ children } : {
+  children: React.ReactNode
+}) => {
+  return (
+    <div className="bg-indigo-500 h-full flex items-center justify-center">
+      {children}
+    </div>
+  );
+}
+
+export default AuthLayout;
