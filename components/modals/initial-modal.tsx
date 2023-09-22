b823cc894d40f93c51fd59ffc0594b972efdff4a
ivandohan <srgivando@gmail.com>
2023-09-23 06:07:32 +0700

    Uploadthing setup, server creation API, and initial navigation sidebar

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,8 +1,11 @@
 "use client"
 
+import axios from "axios"
 import * as z from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
+import { useEffect, useState } from "react"
+
 
 import {
   Dialog,
@@ -22,7 +25,8 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
-import { useEffect, useState } from "react"
+import { FileUpload } from "@/components/file-upload"
+import { useRouter } from "next/navigation"
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -35,6 +39,8 @@ const formSchema = z.object({
 
 export const InitialModal = () => {
   const [isMounted, setIsMounted] = useState(false)
+
+  const router = useRouter()
 
   useEffect(() => {
     setIsMounted(true)
@@ -51,7 +57,14 @@ export const InitialModal = () => {
   const isLoading = form.formState.isSubmitting
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values)
+    try {
+      await axios.post("/api/servers", values);
+      form.reset()
+      router.refresh()
+      window.location.reload()
+    } catch(error) {
+      console.log(error)
+    }
   }
 
   if(!isMounted) {
@@ -63,7 +76,7 @@ export const InitialModal = () => {
       <DialogContent className="bg-white text-black p-0 overflow-hidden">
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl text-center font-bold">
-            Customize your duck!
+            Customize your server!
           </DialogTitle>
           <DialogDescription className="text-center text-zinc-500">
             Give your server a personality with a name and an image.
@@ -75,7 +88,21 @@ export const InitialModal = () => {
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <div className="space-y-8 px-6">
               <div className="flex items-center justify-center text-center">
-                TODO: Image Upload
+                <FormField 
+                  control={form.control}
+                  name="imageUrl"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <FileUpload
+                          endpoint="serverImage"
+                          value={field.value}
+                          onChange={field.onChange}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
               </div>
 
               <FormField 
