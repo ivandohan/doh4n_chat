49ba3181dfbab3c6a9cae0142cb6849fd0b89d0f
ivandohan <srgivando@gmail.com>
2023-10-06 23:17:47 +0700

    (main) Layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -4,7 +4,7 @@ const MainLayout = async ({ children }: {
   children: React.ReactNode
 }) => {
   return (
-    <div className="h-full">
+    <div className="h-full okoklh">
       <div
         className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0"
       >
