6150959eab25e189b664e329605dc0c8311ab5bf
ivandohan <srgivando@gmail.com>
2023-10-07 23:06:58 +0700

    M

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -6,7 +6,7 @@ const MainLayout = async ({ children }: {
   return (
     <div className="h-full okoklh">
       <div
-        className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0"
+        className="hidden md:flex h-full w-[73px] z-30 flex-col fixed inset-y-0"
       >
         <NavigationSideBar />
       </div>
